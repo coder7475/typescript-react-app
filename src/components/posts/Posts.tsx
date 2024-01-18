d1e976e5053bc6999290741fb5503ee88b2190db
coder7475 <robiulhossain7475@gmail.com>
2024-01-18 11:48:57 +0600

    feat: show fetched posts data using mui x data grid

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Post from "../../types/Post";
 import fetchPosts from "../../utils/fetchPosts";
-
+import { DataGrid  } from '@mui/x-data-grid';
 const Posts = () => {
     const [posts, setPosts] = useState<Post[]>([]);
 
@@ -13,12 +13,17 @@ const Posts = () => {
     
     }, []);
 
-    console.log(posts);
+    // console.log(posts);
     
+    const columns = [
+        { field: 'id', headerName: 'ID' },
+        { field: 'title', headerName: 'Title', width: 300 },
+        { field: 'body', headerName: 'Body', width: 700 }
+    ]
 
     return (
-        <div>
-            All Posts here.
+        <div style={{ height: 600, width: '100%' }}>
+            <DataGrid rows={posts} columns={columns} autoPageSize/>
         </div>
     );
 };
