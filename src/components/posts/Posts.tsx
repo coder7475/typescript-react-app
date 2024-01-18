28ca48ff1a1d1576f9f069d42413c34b6f3717e8
coder7475 <robiulhossain7475@gmail.com>
2024-01-18 11:10:11 +0600

    feat: fetch the post in second page

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -1,6 +1,21 @@
+import { useEffect, useState } from "react";
+import Post from "../../types/Post";
+import fetchPosts from "../../utils/fetchPosts";
 
 const Posts = () => {
+    const [posts, setPosts] = useState<Post[]>([]);
+
+    useEffect(() => {
+      fetchPosts()
+        .then(data => {
+            setPosts(data);
+        })
     
+    }, []);
+
+    console.log(posts);
+    
+
     return (
         <div>
             All Posts here.
